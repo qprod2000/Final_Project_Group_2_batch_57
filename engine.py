4ec7f9b268fbcd05d1fa4cb00257d4321d143b57
Hengkie Wirawijaya <109408642+qprod2000@users.noreply.github.com>
2026-04-17 02:03:58 +0000

    update engine.py

--- engine.py
+++ engine.py
@@ -1,28 +1,23 @@
 import pandas as pd # type: ignore
-import numpy as np  # type: ignore
+import joblib   # type: ignore  
 
 def find_best_flights(df, model, input_data, preference=50):
     df = df.copy()
 
-    # Sampling biar cepat
     df = df.sample(min(300, len(df)), random_state=42)
 
-    # Predict price
-    df["predicted_price"] = model.predict(df[input_data.keys()])
+    df["price"] = model.predict(df[model.feature_names_in_])
 
-    # Baseline (untuk perbandingan)
     base_price = df["price"].mean()
     base_duration = df["duration"].mean()
 
-    # Hitung value score
     penalty = 20 + (100 - preference)
 
-    df["price_saving"] = base_price - df["price"]
-    df["extra_time"] = df["duration"] - base_duration
+    df["value_score"] = (
+        (base_price - df["price"]) -
+        ((df["duration"] - base_duration) * penalty)
+    )
 
-    df["value_score"] = df["price_saving"] - (df["extra_time"] * penalty)
-
-    # Pisahkan kelas
     eco = df[df["class"] == "Economy"].sort_values("value_score", ascending=False).head(3)
     biz = df[df["class"] == "Business"].sort_values("value_score", ascending=False).head(3)
 
@@ -36,4 +31,4 @@ def explain(row, base_price, base_duration):
     if saving > 0:
         return f"Hemat ₹{int(saving)} (+{extra_time:.1f} jam)"
     else:
-        return f"Lebih cepat ({abs(extra_time):.1f} jam lebih singkat)"
+        return f"Lebih cepat {abs(extra_time):.1f} jam"
