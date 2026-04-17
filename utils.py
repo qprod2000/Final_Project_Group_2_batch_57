359872c1806d8fcb6c95652b8b1b165b4cd4841a
Hengkie Wirawijaya <109408642+qprod2000@users.noreply.github.com>
2026-04-17 01:34:58 +0000

    insert util.py

---
+++ utils.py
@@ -0,0 +1,19 @@
+def normalize_stops(x):
+    x = str(x).lower()
+
+    if x in ["0 stops", "zero", "non-stop"]:
+        return "Langsung"
+    elif x in ["1 stop", "one"]:
+        return "1 Transit"
+    else:
+        return "2 Transit"
+
+
+def format_duration(x):
+    h = int(x)
+    m = int((x - h) * 60)
+    return f"{h} jam {m} menit"
+
+
+def format_inr(x):
+    return f"₹ {int(x):,}"
